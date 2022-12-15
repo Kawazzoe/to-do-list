f3ad08457847abdeacc3aa780301ec890f33ac59
viniciusk703@gmail.com <viniciusk703@gmail.com>
2022-12-15 09:41:04 -0300

    15/12/2022

---
+++ src/components/Base/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-navigation-drawer v-model="$parent.drawer" app>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6">
+          Lista {{ nome.length ? `de ${nome}` : "do Kauan" }}
+        </v-list-item-title>
+        <v-list-item-subtitle> Lista de tarefas </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense nav>
+      <v-list-item v-for="item in items" :key="item.title" link :to="item.route">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "NavBar",
+  data: () => ({
+    items: [
+      { title: "Lista", icon: "mdi-list-box-outline", route: "/" },
+      { title: "Sobre", icon: "mdi-help-box", route: "/about" },
+    ],
+  }),
+  computed: {
+    ...mapState("todo", ["nome"]),
+  },
+};
+</script>
+
+<style>
+
+</style>
